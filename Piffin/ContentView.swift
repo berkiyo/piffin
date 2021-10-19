118e6a0117813b906692bb04b5ecb98d4b69113f
berkiyo <berk@tekbyte.net>
2021-10-19 20:55:20 +1100

    initial commit

---
+++ Piffin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Piffin
+//
+//  Created by Berk Dogan on 19/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Piffin/PiffinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiffinApp.swift
+//  Piffin
+//
+//  Created by Berk Dogan on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PiffinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
