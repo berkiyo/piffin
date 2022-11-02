74ac08e760380c15c8a5c49c0e2ab4c33b852330
berkiyo <berk@tekbyte.net>
2022-11-02 20:21:50 +1100

    Test change using Working Copy + Textastic

--- Piffin.swiftpm/ContentView.swift
+++ Piffin.swiftpm/ContentView.swift
@@ -1,34 +1,63 @@
-import SwiftUI
+/**
+ This is the main content.
+ Copyright Berk Dogan, 2022.
+ */
 
+import SwiftUI
+		
 struct ContentView: View {
     @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
-        VStack(alignment: .leading) {
+        
+        NavigationView {
             
-            /**
-             Incrementor
-             */
-            Button("Tap count: \(tapCount)") {
-                tapCount += 1
-                UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+            VStack(alignment: .center) {
+                
+                HStack {
+                    
+                    Text("Counter = \(tapCount)")
+                        .font(.system(size: 32, weight: .heavy))
+                }
+                
+                HStack {
+                    Spacer()
+                    /**
+                     Incrementor
+                     */
+                    Button("Increment") {
+                        tapCount += 1
+                        UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                    }
+                    .padding()
+                    .buttonStyle(GrowingButton())
+                    /**
+                     Decrement
+                     */
+                    Button("Decrement") {
+                        if tapCount == 0 {
+                            tapCount = 0
+                        } else {
+                            tapCount -= 1
+                        }
+                        
+                        UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                        
+                    }
+                    .buttonStyle(GrowingButton())
+                    Spacer()
+                }
+                /**
+                 Clear all data
+                 */
+                Button("Reset Counter") {
+                    tapCount = 0
+                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                }
+                .buttonStyle(GrowingButton())
             }
-            
-            /**
-             Decrement
-             */
-            Button("Decrement") {
-                tapCount -= 1
-                UserDefaults.standard.set(self.tapCount, forKey: "Tap")
-            }
-            
-            /**
-             Clear all data
-             */
-            Button("Clear") {
-                tapCount = 0
-                UserDefaults.standard.set(self.tapCount, forKey: "Tap")
-            }
+            .navigationTitle("Piffin")
         }
+        
     }
 }
